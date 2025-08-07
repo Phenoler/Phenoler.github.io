8173d0cf831a4bfdc29cb93a7c3f305cbcc6572e
Phenol <158245074+Phenoler@users.noreply.github.com>
2025-08-07 21:33:03 +0800

    Delete dist directory

--- dist/assets/HelloWorld-CLnqJ51j.js
+++
@@ -1,9 +0,0 @@
-var e=`---
-title: HelloWorld
-date: 2024-07-20 18:46:08
-tags: test
----
-
-HelloWorld
-
-`;export{e as default};
--- dist/assets/博客重构测试-DxvADhAb.js
+++
@@ -1,7 +0,0 @@
-var e=`---\r
-title: 博客重构测试\r
-date: 2025-08-07 18:46:08\r
-tags: test\r
----\r
-\r
-Hello World !`;export{e as default};
