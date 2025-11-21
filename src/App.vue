d0df902fe58f02e668598b297da7ef3bc310cca7
Phenoler <17314455965@163.com>
2025-11-21 19:10:43 +0800

    修复主题切换但是背景不会跟随切换的bug

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <template>
-  <NavBar />
-  <router-view />
+  <div class="app-container">
+    <NavBar />
+    <router-view />
+  </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar/NavBar.vue'
+import { useTheme } from './utils/useTheme'
+import { onMounted } from 'vue'
 
 export default {
   name: 'App',
   components: {
     NavBar
+  },
+  setup() {
+    const { loadTheme } = useTheme()
+    
+    // 页面加载时立即应用保存的主题
+    onMounted(() => {
+      loadTheme()
+    })
+    
+    return {}
   }
 }
 </script>
+
+<style>
+.app-container {
+  background-color: var(--bg-color);
+  min-height: 100vh;
+  transition: background-color 0.3s ease;
+}
+</style>
 
